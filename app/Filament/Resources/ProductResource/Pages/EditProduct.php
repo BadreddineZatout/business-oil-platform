2f529be75ff40bcd8027fb0df0087e6fa51752c0
BadreddineZatout <hb_zatout@esi.dz>
2023-09-29 16:33:43 +0100

    feat: product resource

--- app/Filament/Resources/ProductResource/Pages/EditProduct.php
+++ app/Filament/Resources/ProductResource/Pages/EditProduct.php
@@ -1,14 +1,14 @@
 <?php
 
-namespace App\Filament\Resources\SupplierResource\Pages;
+namespace App\Filament\Resources\ProductResource\Pages;
 
-use App\Filament\Resources\SupplierResource;
+use App\Filament\Resources\ProductResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
-class EditSupplier extends EditRecord
+class EditProduct extends EditRecord
 {
-    protected static string $resource = SupplierResource::class;
+    protected static string $resource = ProductResource::class;
 
     protected function getHeaderActions(): array
     {
